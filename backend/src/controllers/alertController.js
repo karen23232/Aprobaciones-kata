5562df824b66c6c8b34752d69eba18f7136d99ec
karen1283 <kruiz14@bancodebogota.com.co>
2025-12-01 21:42:44 -0500

    onbradinr

--- backend/src/controllers/alertController.js
+++ backend/src/controllers/alertController.js
@@ -2,7 +2,7 @@ const {
   checkAndSendAlerts,
   sendManualAlert,
   resetAlert
-} = require('../services/onboardingAlertService.js');
+} = require('../services/onboardingalertservice.js');
 const Employee = require('../models/Employee.js');
 
 // @desc    Verificar y enviar alertas automáticamente
